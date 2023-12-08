748d0c16149bb26c3903329683f4aa29f869ac3d
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-12-08 10:35:05 +0900

    .

---
+++ SinhanSol/app/src/main/java/com/example/sinhansol/EasyHomeFragment.java
@@ -0,0 +1,16 @@
+package com.example.sinhansol;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class EasyHomeFragment extends Fragment {
+
+
+}
